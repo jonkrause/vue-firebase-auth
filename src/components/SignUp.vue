3494e12faa75c2535253785d85c9be3b766275a5
jonkrause <jonathonkrause@gmail.com>
2018-04-18 18:01:53 -0400

    working auth boilerplate

---
+++ src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="signup">
+    <h3>Sign Up</h3>
+    <input type="text" placeholder="Email" v-model="email"><br>
+    <input type="password" placeholder="Password" v-model="password"><br>
+    <button @click="signUp">Sign Up</button>
+    <p>Already have an account? <router-link to="/login">login</router-link></p>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signUp() {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+          user => {
+            this.$router.replace('home')
+          },
+          function(err) {
+            alert('There was a problem creating your account' + err.message)
+          }
+        )
+    }
+  }
+}
+</script>
+
+<style>
+.signup {
+  display: block;
+  text-align: center;
+}
+</style>
