dbe01f4532d60d5d477b3d09be59a0a495816505
jonkrause <jonathonkrause@gmail.com>
2018-04-18 18:09:04 -0400

    spelling correction

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,7 +4,7 @@
     <input type="text" placeholder="Email" v-model="email"><br>
     <input type="password" placeholder="Password" v-model="password"><br>
     <button @click="signUp">Sign Up</button>
-    <p>Already have an account? <router-link to="/login">login</router-link></p>
+    <p>Already have an account? <router-link to="/login">Login</router-link></p>
   </div>
 </template>
 
